6a7a6181d768fc7885a1b08c780dc43251b41f31
Jack Bader <jack@culpritunderwear.com>
2024-04-29 22:53:57 -0400

    updates to api

--- app.mjs
+++ app.mjs
@@ -1,15 +1,43 @@
 import express from "express";
 import cache from "./cache.mjs";
 import { v4 as uuidv4 } from "uuid";
+import cors from "cors";
+import dotenv from 'dotenv';
+import pg from 'pg';
+import fs from 'fs';
+
+dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const dbPassword = process.env.DATABASE_PASSWORD;
+const { Pool } = pg;
+const pool = new Pool({
+  user: 'avnadmin',
+  host: 'pg-6f5c9083-f3da-46b6-8e0e-5059b0eeeadc-booksda2350796403-chore.h.aivencloud.com',
+  database: 'defaultdb',
+  password: dbPassword,
+  port: 27760,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync('./postgres-ca.pem').toString(),
+  },
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error('Connection error', err.stack);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
 // add a book - request body should contain a title, status and an author
-app.post("/reading-list/books", (req, res) => {
+app.post("/reading-list/books", async (req, res) => {
   const { title, author, status } = req.body;
-  const uuid = uuidv4();
   if (!(status === "read" || status === "to_read" || status === "reading")) {
     return res.status(400).json({
       error: "Status is invalid. Accepted statuses: read | to_read | reading",
@@ -18,9 +46,15 @@ app.post("/reading-list/books", (req, res) => {
   if (!title || !author || !status) {
     return res.status(400).json({ error: "Title, Status or Author is empty" });
   }
-  const value = { uuid, title, author, status };
-  cache.set(uuid, value, 86400);
-  return res.status(201).json({ uuid, title, author });
+  const value = {  title, author, status };
+
+  try {
+    await pool.query('INSERT INTO books (title, author, status) VALUES ($1, $2, $3)', [title, author, status]);
+    return res.status(201).json(value);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Database error' });
+  }
 });
 
 // update status of a book by uuid
@@ -45,13 +79,14 @@ app.put("/reading-list/books/:uuid", (req, res) => {
 });
 
 // get the list of books
-app.get("/reading-list/books", (_, res) => {
-  const keys = cache.keys();
-  const allData = {};
-  for (const key of keys) {
-    allData[key] = cache.get(key);
+app.get("/reading-list/books", async (_, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM books');
+    return res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Database error' });
   }
-  return res.json(allData);
 });
 
 // get a book by uuid
@@ -68,16 +103,22 @@ app.get("/reading-list/books/:uuid", (req, res) => {
 });
 
 // delete a book by uuid
-app.delete("/reading-list/books/:uuid", (req, res) => {
+app.delete("/reading-list/books/:uuid", async (req, res) => {
   const uuid = req.params.uuid;
   if (!uuid || typeof uuid !== "string") {
     return res.status(400).json({ error: "missing or invalid UUID" });
   }
-  if (!cache.has(uuid)) {
-    return res.status(404).json({ error: "UUID does not exist" });
+
+  try {
+    const result = await pool.query('DELETE FROM books WHERE id = $1 RETURNING *', [uuid]);
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "UUID does not exist" });
+    }
+    return res.json({ uuid });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Database error' });
   }
-  cache.del(uuid);
-  return res.json({ uuid });
 });
 
 // health check
---
+++ migrations/1714443319503_create-books-table.cjs
@@ -0,0 +1,27 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.createTable('books', {
+        id: 'id',
+        title: { type: 'text', notNull: true },
+        author: { type: 'text', notNull: true },
+        status: { type: 'text', notNull: true },
+      });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropTable('books');
+};
